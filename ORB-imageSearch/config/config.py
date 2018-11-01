22d7616dbd167fc6f051716970b76f7183f0ef2a
Mousse <zibuyu1995@gmail.com>
2018-11-02 00:19:46 +0800

    feat(imageSearch): add image search

--- ORB-imageSearch/config/config.py
+++ ORB-imageSearch/config/config.py
@@ -9,7 +9,7 @@ cpu_count = cpu_count()
 
 dataset_db_path = os.path.join(project_path, 'static/dataset.db')
 dataset_path = os.path.join(project_path, 'static/dataset/')
-maximum_features = 100  # 图像最大特征点
+maximum_features = 618  # 图像最大特征点
 scale_factor = 1.2  # 缩放因子
 match_type = 1  # 1 knn, 2 bf
 
