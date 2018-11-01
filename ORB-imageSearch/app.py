56079495bbbd83d020c566686a65eb0ce1d95289
Mousse <zibuyu1995@gmail.com>
2018-11-02 00:19:46 +0800

    feat(imageSearch): add image search

--- ORB-imageSearch/app.py
+++ ORB-imageSearch/app.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import asyncio
+from operator import itemgetter
+
+import msgpack
+import uvloop
+from config.config import (
+    dataset_db_path
+)
+from config.config import match_type
 from sanic import Sanic
 from sanic.response import json
+from until.orb_features import generate_image_feature
 from until.orb_matches import knn_match, bf_match
-from config.config import match_type
 
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 app = Sanic(__name__)
 
@@ -20,24 +30,27 @@ async def search():
     pass
 
 
-async def image_search(eve_loop, processes_executor, images_features):
-    """ 多进程匹配 """
-    image_search_tasks = []
-    task_append = image_search_tasks.append
-    for image_feature in images_feature:
+def image_search(images_features=None):
+    """ 图像搜索 """
+    with open(dataset_db_path, 'rb') as db:
+        images_feature = msgpack.load(db, use_list=False)
+    import time
+    a = time.time()
+    match_results = []
+    match_results_append = match_results.append
+    search_feature = generate_image_feature('./static/dataset/100900.png')[1]
+    for image_uid, feature in images_feature.items():
         if match_type == 1:
-            task_append(
-                eve_loop.run_in_executor(
-                    processes_executor, knn_match, image_feature
-                )
-            )
+            match_degree = knn_match(search_feature, feature)
+            if match_degree < 11:
+                continue
         else:
-            task_append(
-                eve_loop.run_in_executor(
-                    processes_executor, bf_match, image_path
-                )
-            )
-    task_results, _ = await asyncio.wait(image_feature_tasks)
+            match_degree = bf_match(search_feature, feature)
+            if match_degree < 11:
+                continue
+        match_results_append((image_uid, match_degree))
+    match_results = sorted(match_results, key=itemgetter(1), reverse=True)
+    return match_results
 
 
 app.run(host='0.0.0.0', port=8000, debug=True)
--- ORB-imageSearch/until/orb_matches.py
+++ ORB-imageSearch/until/orb_matches.py
@@ -1,20 +1,23 @@
 # coding: utf-8
 
+import cv2
+import numpy as np
 
-def knn_match(feature_1, feature_2):
+
+def knn_match(search_feature, feature):
     """ knn 匹配 """
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-    matches = bf.knnMatch(feature_1, trainDescriptors=feature_2, k=2)
+    search_feature = np.array(search_feature, dtype='uint8')
+    feature = np.array(feature, dtype='uint8')
+    bf_matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
+    matches = bf_matcher.knnMatch(search_feature, trainDescriptors=feature, k=2)
     good_matches = [m for (m, n) in matches if m.distance < 0.75 * n.distance]
     return len(good_matches)
 
 
-def bf_match(feature_1, feature_2):
+def bf_match(search_feature, feature):
     """ 蛮力匹配(保留最大的特征点数目) """
-
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    matches = bf.match(feature_1, feature_2)
+    search_feature = np.array(search_feature, dtype='uint8')
+    feature = np.array(feature, dtype='uint8')
+    bf_matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bf_matcher.match(search_feature, feature)
     return len(matches)
-
-
-
