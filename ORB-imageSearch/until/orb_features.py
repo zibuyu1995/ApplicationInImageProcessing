421acfc2400a0c4f0c84d28cdf411488cdb1a00f
Mousse <zibuyu1995@gmail.com>
2018-11-02 23:50:57 +0800

    feat(feature): new feature persist model

--- ORB-imageSearch/until/orb_features.py
+++ ORB-imageSearch/until/orb_features.py
@@ -1,17 +1,22 @@
 # coding: utf-8
 
 import cv2
+import msgpack
 from config.config import maximum_features, scale_factor
 
 
-def generate_image_feature(image_path: str) -> tuple:
+def generate_image_feature(image_path: str, is_dumps: bool) -> tuple:
     """
     生成图像特征点
+    :param is_dumps: 是否压缩
     :param image_path: 图像路径
     """
     image_uid = image_path[image_path.rfind("/") + 1:][:6]
     images = cv2.imread(image_path)
     orb = cv2.ORB_create(maximum_features, scale_factor)
     (kp, des) = orb.detectAndCompute(images, None)
-    feature = des.tolist()
+    if is_dumps:
+        feature = des.tolist()
+    else:
+        feature = des
     return image_uid, feature
--- ORB-imageSearch/until/orb_matches.py
+++ ORB-imageSearch/until/orb_matches.py
@@ -6,7 +6,6 @@ import numpy as np
 
 def knn_match(search_feature, feature):
     """ knn 匹配 """
-    search_feature = np.array(search_feature, dtype='uint8')
     feature = np.array(feature, dtype='uint8')
     bf_matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
     matches = bf_matcher.knnMatch(search_feature, trainDescriptors=feature, k=2)
@@ -16,7 +15,6 @@ def knn_match(search_feature, feature):
 
 def bf_match(search_feature, feature):
     """ 蛮力匹配(保留最大的特征点数目) """
-    search_feature = np.array(search_feature, dtype='uint8')
     feature = np.array(feature, dtype='uint8')
     bf_matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
     matches = bf_matcher.match(search_feature, feature)
